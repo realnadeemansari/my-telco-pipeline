b8d6d41203d097eccc541effe45f5db59f11f602
Mohd Nadeem <mohdnadeem@Mohds-MacBook-Air.local>
2026-07-05 20:23:15 +0530

    Modified the code of lambda fucntion

--- cdk-infra/cdk_infra/resources/lambda_function.py
+++ cdk-infra/cdk_infra/resources/lambda_function.py
@@ -9,19 +9,18 @@ ENDPOINT_NAME = os.environ["ENDPOINT_NAME"]
 
 def lambda_handler(event, context):
 
-    payload = json.dumps(event)
+    payload = event["body"]
 
     response = runtime.invoke_endpoint(
         EndpointName=ENDPOINT_NAME,
         ContentType="application/json",
         Body=payload
     )
-
     prediction = json.loads(
         response["Body"].read().decode("utf-8")
     )
 
     return {
         "statusCode": 200,
-        "body": prediction
+        "body": json.dumps(prediction)
     }
